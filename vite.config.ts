9554c8e5e76e1c19107ad60998a609a9657e304f
HEAZYLENS611 <hazelmabote611@gmail.com>
2026-02-06 19:15:28 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { componentTagger } from 'lovable-tagger'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // GitHub Pages friendly: use '/' for custom domains to avoid white screen issues.
-  base: 'PAGINA-DE-VENDAS-APOSENTE-',
+  base: 'Aposente-O-Esfor-o-Manual',
   server: {
     port: 8080,
   },
